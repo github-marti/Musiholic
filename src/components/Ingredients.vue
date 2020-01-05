1b786a0df0d1355c7955bb7256ecaef0a9d83dd5
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-05 13:33:45 -0800

    formatted checkboxes and made clicking them update the state

--- src/components/Ingredients.vue
+++ src/components/Ingredients.vue
@@ -2,42 +2,54 @@
   <div class="ingredients mx-auto">
     <p class="title mb-1 p-2 font-italic">Let me make you something to drink...</p>
     <div class="card p-3">
-      <p class="mb-1">
-        <strong>Pick your poison</strong>
-      </p>
-      <div class="dropdown">
-        <button
-          class="btn dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >Base Alcohol</button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a
-            class="dropdown-item"
-            href="#"
-            v-for="drink in drinks"
-            :key="drink.id"
-            v-on:click="select"
-          >
-            <span>{{drink.name}}</span>
-          </a>
+      <div class="mx-auto choose-alochol">
+        <p class="mb-1">
+          <strong>Pick your poison</strong>
+        </p>
+        <div class="dropdown">
+          <button
+            class="btn dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >Base Alcohol</button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a
+              class="dropdown-item"
+              href="#"
+              v-for="drink in drinks"
+              :key="drink.id"
+              v-on:click="select"
+            >
+              <span>{{drink.name}}</span>
+            </a>
+          </div>
         </div>
       </div>
-      <div class="checkbox-container mt-3">
-        <div
-          v-for="addon in addons"
-          :key="addon.id"
-        >
+      <div class="checkbox-container mt-4">
+        <div class="group1">
+          <div class="checkbox" v-for="addon in addons.group1" :key="addon.id">
             <input
-            type="checkbox"
-            class="checkbox-item"
-            v-model="checked"
-            v-bind:value="addon.name"
-            >
-            <label>{{ addon.name }}</label>
+              type="checkbox"
+              class="checkbox-item"
+              v-bind:value="addon.name"
+              v-on:click="check"
+            />
+            <label class="px-1">{{ addon.name }}</label>
+          </div>
+        </div>
+        <div class="group2">
+          <div class="checkbox" v-for="addon in addons.group2" :key="addon.id">
+            <input
+              type="checkbox"
+              class="checkbox-item"
+              v-bind:value="addon.name"
+              v-on:click="check"
+            />
+            <label class="px-1">{{ addon.name }}</label>
+          </div>
         </div>
       </div>
     </div>
@@ -45,22 +57,36 @@
 </template>
 
 <script>
-import ingredients from '../utils/ingredients'
+import ingredients from "../utils/ingredients";
 
 export default {
   name: "Ingredients",
   data() {
     return {
-      selected: {},
+      selected: {
+          drink: '',
+          addons: []
+      },
       drinks: ingredients.drinks,
-      addons: ingredients.addons
+      addons: {
+        group1: ingredients.addons.group1,
+        group2: ingredients.addons.group2
+      }
     };
   },
   methods: {
     select: function(event) {
       document.getElementById("dropdownMenuButton").textContent =
         event.target.textContent;
-      this.selected.drink = event.target.textContent.replace(/\s/g, '%20');
+      this.selected.drink = event.target.textContent.replace(/\s/g, "%20");
+    },
+    check: function(event) {
+        console.log(event.target.value);
+        if (this.selected.addons.indexOf(event.target.value) === -1) {
+            this.selected.addons.push(event.target.value);
+        } else {
+            this.selected.addons = this.selected.addons.filter(x => x !== event.target.value);
+        };
     }
   }
 };
@@ -84,8 +110,8 @@ export default {
   font-size: 0.8em;
 }
 
-/* .checkbox-container {
+.checkbox-container {
   display: flex;
-  flex-wrap: wrap;
-} */
+  justify-content: space-evenly;
+}
 </style>
--- src/utils/ingredients.js
+++ src/utils/ingredients.js
@@ -13,22 +13,26 @@ export default {
     { name: "Lager", id: 11 },
     { name: "Beer", id: 12 }
   ],
-  addons: [
-    { name: "Club Soda", id: 1 },
-    { name: "Tonic", id: 2 },
-    { name: "Cranberry Juice", id: 3 },
-    { name: "Pineapple Juice", id: 4 },
-    { name: "Orange Juice", id: 5 },
-    { name: "Lime Juice", id: 6 },
-    { name: "Lemon Juice", id: 7 },
-    { name: "Vermouth", id: 8 },
-    { name: "Simple Syrup", id: 9 },
-    { name: "Cream", id: 10 },
-    { name: "Egg White", id: 11 },
-    { name: "Ginger", id: 12 },
-    { name: "Orange Peel", id: 13 },
-    { name: "Mint", id: 14 },
-    { name: "Lemon", id: 15 },
-    { name: "Lime", id: 16 },
-  ]
+  addons: {
+    group1: [
+      { name: "Club Soda", id: 1 },
+      { name: "Tonic", id: 2 },
+      { name: "Cranberry Juice", id: 3 },
+      { name: "Pineapple Juice", id: 4 },
+      { name: "Orange Juice", id: 5 },
+      { name: "Lime Juice", id: 6 },
+      { name: "Lemon Juice", id: 7 },
+      { name: "Vermouth", id: 8 }
+    ],
+    group2: [
+      { name: "Simple Syrup", id: 9 },
+      { name: "Cream", id: 10 },
+      { name: "Egg White", id: 11 },
+      { name: "Ginger", id: 12 },
+      { name: "Orange Peel", id: 13 },
+      { name: "Mint", id: 14 },
+      { name: "Lemon", id: 15 },
+      { name: "Lime", id: 16 }
+    ]
+  }
 }
