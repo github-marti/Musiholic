914fa7ed61f0149001463f14a3aa068d39c7bda9
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-05 12:37:56 -0800

    added state management to track selected drink/ingredients

--- src/components/Ingredients.vue
+++ src/components/Ingredients.vue
@@ -15,13 +15,14 @@
           aria-expanded="false"
         >Base Alcohol</button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a 
-            class="dropdown-item" 
-            href="#" 
-            v-for="drink in drinks" 
+          <a
+            class="dropdown-item"
+            href="#"
+            v-for="drink in drinks"
             :key="drink.id"
-            v-on:click="select">
-              <span>{{drink.name}}</span>
+            v-on:click="select"
+          >
+            <span>{{drink.name}}</span>
           </a>
         </div>
       </div>
@@ -34,45 +35,50 @@ export default {
   name: "Ingredients",
   data() {
     return {
+      selected: {},
       drinks: [
-        {name: "Vodka", id: 1}, 
-        {name: "Gin", id: 2},
-        {name: "Tequila", id: 3},
-        {name: "Dark Rum", id: 4},
-        {name: "Light Rum", id: 5},
-        {name: "Rye Whiskey", id: 6},
-        {name: "Bourbon", id: 7},
-        {name: "Red Wine", id: 8},
-        {name: "White Wine", id: 9},
-        {name: "Champagne", id: 10},
-        {name: "Lager", id: 11},
-        {name: "Beer", id: 12}
-        ]
+        { name: "Vodka", id: 1 },
+        { name: "Gin", id: 2 },
+        { name: "Tequila", id: 3 },
+        { name: "Dark Rum", id: 4 },
+        { name: "Light Rum", id: 5 },
+        { name: "Rye Whiskey", id: 6 },
+        { name: "Bourbon", id: 7 },
+        { name: "Red Wine", id: 8 },
+        { name: "White Wine", id: 9 },
+        { name: "Champagne", id: 10 },
+        { name: "Lager", id: 11 },
+        { name: "Beer", id: 12 }
+      ]
     };
   },
   methods: {
-      select: function (event) {
-          document.getElementById('dropdownMenuButton').textContent = event.target.textContent;
-      }
+    select: function(event) {
+      console.log(this.selected);
+      document.getElementById("dropdownMenuButton").textContent =
+        event.target.textContent;
+      this.selected.drink = event.target.textContent;
+      console.log(this.selected);
+    }
   }
 };
 </script>
 
 <style>
 .title {
-    font-size: 1.4em;
+  font-size: 1.4em;
 }
 
 .ingredients {
-    width: 85%;
+  width: 85%;
 }
 
 .dropdown button {
-    border: 1px solid #ccc;
-    font-size: .8em;
+  border: 1px solid #ccc;
+  font-size: 0.8em;
 }
 
 .dropdown-item {
-    font-size: .8em;
+  font-size: 0.8em;
 }
 </style>
