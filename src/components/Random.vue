d1f26ccc25965dc5876cc79c77c89bd5b41bb30e
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-06 21:50:10 -0800

    added styling to genre blocks

--- src/components/Random.vue
+++ src/components/Random.vue
@@ -3,12 +3,24 @@
     <p class="title mb-1 p-2 font-bold">Match your musical mood:</p>
     <div class="genre-container">
         <div class="row">
-            <div class="col-6 col-sm-4 genre-block">Rock</div>
-            <div class="col-6 col-sm-4 genre-block">Pop</div>
-            <div class="col-6 col-sm-4 genre-block">Hip Hop</div>
-            <div class="col-6 col-sm-4 genre-block">Country</div>
-            <div class="col-6 col-sm-4 genre-block">Dance</div>
-            <div class="col-6 col-sm-4 genre-block">Indie</div>
+            <div class="col-6 col-sm-4">
+                <div class="genre-block">Rock</div>
+            </div>
+            <div class="col-6 col-sm-4">
+                <div class="genre-block">Pop</div>
+            </div>
+            <div class="col-6 col-sm-4">
+                <div class="genre-block">Hip Hop</div>
+            </div>
+            <div class="col-6 col-sm-4">
+                <div class="genre-block">Country</div>
+            </div>
+            <div class="col-6 col-sm-4">
+                <div class="genre-block">Dance</div>
+            </div>
+            <div class="col-6 col-sm-4">
+                <div class="genre-block">Indie</div>
+            </div>
         </div>
     </div>
   </div>
@@ -23,5 +35,12 @@ export default {
 <style>
 .random {
   width: 85%;
+}
+
+.genre-block {
+    text-align: center;
+    padding: 30px 10px;
+    border: 1px solid #ccc;
+    margin: 5px 0;
 }
 </style>
