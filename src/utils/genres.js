c5de61ec22b0f55a5be75c4bae4cf03aece7f7eb
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-06 21:54:20 -0800

    populated genres js file

---
+++ src/utils/genres.js
@@ -0,0 +1,10 @@
+export default {
+    genres: [
+        { name: "Rock", id: 1},
+        { name: "Pop", id: 2},
+        { name: "Country", id: 3 },
+        { name: "Hip Hop", id: 4 },
+        { name: "Dance", id: 5 },
+        { name: "Indie", id: 6 }
+    ]
+}
