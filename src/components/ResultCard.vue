d9e8c763fd325358d60c50342dd4034e1256d86f
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-12 15:11:29 -0800

    added conditional to only load 5 if there are more than 5 results

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <div class="result-card card mx-auto my-3 p-3 text-center" v-for="index in 5" :key="results[index].idDrink">
-      <h3>{{ results[index].strDrink }}</h3>
-      <img class="result-img mx-auto" :src="results[index].strDrinkThumb">
-    </div>
+    <template v-if="results === 'None Found' || results.length < 5">
+      <h3>Less than 5</h3>
+    </template>
+    <template v-else>
+      <div
+        class="result-card card mx-auto my-3 p-3 text-center"
+        v-for="index in 5"
+        :key="results[index].idDrink"
+      >
+        <h3>{{ results[index].strDrink }}</h3>
+        <img class="result-img mx-auto" :src="results[index].strDrinkThumb" />
+      </div>
+    </template>
   </div>
 </template>
 
@@ -13,19 +22,22 @@ import { globalState } from "../main.js";
 export default {
   name: "ResultCard",
   data() {
-      return {
-          results: globalState.results
-      }
+    return {
+      results: globalState.results
+    };
+  },
+  created() {
+      console.log(globalState.results);
   }
 };
 </script>
 
 <style>
-    .result-card {
-        width: 70%;
-    }
+.result-card {
+  width: 70%;
+}
 
-    .result-img {
-        width: 33%;
-    }
+.result-img {
+  width: 33%;
+}
 </style>
