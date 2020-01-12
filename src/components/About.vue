7660bad10dab4b1f3801b39b1b0c5208b8c32ac1
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-11 16:57:37 -0800

    created about component for further modalization

---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mx-auto mt-2 p-3 about">
+    <p
+      class="quicksand"
+    >Looking for the perfect cocktail and music combination? Sit back, relax, and open this app! Input your favorite ingredients and pick one of the available drink recipes, and Musi-Holic will recommend a song for you based on your drink of choice.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
+
