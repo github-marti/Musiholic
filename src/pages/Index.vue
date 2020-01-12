815150c673313836462f9d28626aea335b430ca5
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-12 09:41:27 -0800

    added about component to index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,6 @@
 <template>
   <Layout>
-    <div class="mx-auto mt-2 p-3 about">
-    <p class="quicksand">
-      Looking for the perfect cocktail and music combination? Sit back, relax, and open this app! Input your favorite ingredients and pick one of the available drink recipes, and Musi-Holic will recommend a song for you based on your drink of choice.
-    </p>
-    </div>
+    <About />
     <Ingredients />
     <Search />
     <Random />
@@ -12,6 +8,7 @@
 </template>
 
 <script>
+import About from '../components/About'
 import Ingredients from '../components/Ingredients'
 import Search from '../components/Search'
 import Random from '../components/Random'
@@ -20,6 +17,7 @@ export default {
     title: 'musiholic'
   },
   components: {
+    About,
     Ingredients,
     Search,
     Random
