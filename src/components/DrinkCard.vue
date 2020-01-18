42ee1c50829197a8012be683730cd1c7db2ab53b
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-18 09:51:34 -0800

    added instructions card with drink instructions"

--- src/components/DrinkCard.vue
+++ src/components/DrinkCard.vue
@@ -3,22 +3,26 @@
     <h3>{{ drink.strDrink }}</h3>
     <img class="drink-img my-3 mx-auto" :src="drink.strDrinkThumb" />
     <IngredientsCard :ingredients="ingredients" />
+    <InstructionsCard :instructions="instructions" />
   </div>
 </template>
 
 <script>
 import { globalState } from "../main";
 import IngredientsCard from "./IngredientsCard";
+import InstructionsCard from "./InstructionsCard";
 
 export default {
   name: "DrinkCard",
   data() {
     return {
       drink: globalState.selectedDrink,
-      ingredients: []
+      ingredients: [],
+      instructions: globalState.selectedDrink.strInstructions
     };
   },
   created() {
+    console.log(this.drink);
     let i = 1;
     while (this.drink[`strIngredient${i}`]) {
       this.ingredients.push({
@@ -31,7 +35,8 @@ export default {
     return this.ingredients;
   },
   components: {
-    IngredientsCard
+    IngredientsCard,
+    InstructionsCard
   }
 };
 </script>
---
+++ src/components/InstructionsCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="instructions-card card mx-auto">
+        <h4>Instructions</h4>
+        {{ instructions }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InstructionsCard",
+    props: [ 'instructions' ]
+}
+</script>
+
+<style>
+    .instructions-card {
+        border-style: none;
+        width: 80%
+    }
+</style>
