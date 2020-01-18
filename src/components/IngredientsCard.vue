5aa14021ab6dfe685619eb379e8c2e103e50d5ed
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-17 19:55:30 -0800

    added list styling

--- src/components/IngredientsCard.vue
+++ src/components/IngredientsCard.vue
@@ -1,12 +1,20 @@
 <template>
-    <div>
-        <h3>{{ ingredients }}</h3>
-    </div>
+  <div>
+    <ul class="ingredients-list list-group-flush mx-auto px-0" v-for="ingredient in ingredients" :key="ingredient">
+      <li class="list-group-item">{{ ingredient }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "IngredientsCard",
-    props: ["ingredients"]
-}
+  name: "IngredientsCard",
+  props: ["ingredients"]
+};
 </script>
+
+<style>
+.ingredients-list {
+  width: 50%;
+}
+</style>
