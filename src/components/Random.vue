036ace0cd57978897ae81b88b6976559ebbf0688
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-18 12:22:46 -0800

    added hover methods to genre blocks

--- src/components/Random.vue
+++ src/components/Random.vue
@@ -2,40 +2,63 @@
   <div class="random mx-auto">
     <p class="title mb-1 p-1 mb-3 font-bold quicksand">Match your musical mood:</p>
     <div class="genre-container">
-        <div class="row">
-            <div class="col-6 col-sm-4"
-                 v-for="genre in genres"
-                 :key="genre.id"
-                 >
-                <div class="genre-block prompt" :style="{ background: genre.background, color: genre.color }">{{genre.name}}</div>
-            </div>
+      <div class="row">
+        <div class="col-6 col-sm-4" v-for="index in 6" :key="genres[index-1].id">
+          <div
+            class="genre-block prompt"
+            :genre="genres[index-1].name"
+            :style="{ background: genres[index-1].background, color: genres[index-1].color }"
+            @mouseover="onHover"
+            @mouseout="offHover"
+          >{{genres[index-1].name}}</div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import music from '../utils/music'
+import music from "../utils/music";
 export default {
   name: "Random",
   data() {
-      return {
-          genres: music.genres
-      }
+    return {
+      genres: music.genres
+    };
+  },
+  methods: {
+    onHover: function(event) {
+      const genre = event.target.getAttribute('genre');
+      const currentGenre = this.genres.find(e => e.name === genre);
+      event.target.setAttribute("style", `background: white; color: ${currentGenre.background}`)
+    },
+    offHover: function(event) {
+      const genre = event.target.getAttribute('genre');
+      const currentGenre = this.genres.find(e => e.name === genre);
+      event.target.setAttribute("style", `background: ${currentGenre.background}; color: ${currentGenre.color}`)
+    }
   }
 };
 </script>
 
 <style>
 .random {
-  width: 90%;
+  width: 85%;
   margin-bottom: 30px;
 }
 
+.genre-container div {
+  padding-left: 0;
+  padding-right: 0;
+}
+
 .genre-block {
-  font-size: 3em;
-    text-align: center;
-    padding: 30px 10px;
-    margin: 5px 0;
+  font-size: 5vw;
+  text-align: center;
+  padding: 30px 10px;
+}
+
+.genre-block:hover {
+  cursor: pointer;
 }
 </style>
--- src/utils/music.js
+++ src/utils/music.js
@@ -1,10 +1,109 @@
 export default {
     genres: [
-        { name: "Rock", id: 1, background: "#110133", color: "white" },
-        { name: "Pop", id: 2, background: "#293462", color: "white" },
-        { name: "Country", id: 3, background: "#4b8e8d", color: "white" },
-        { name: "Hip Hop", id: 4, background: "#00918e", color: "white" },
-        { name: "Dance", id: 5, background: "#4dd599", color: "#474747"},
-        { name: "Indie", id: 6, background: "#a8ffa3", color: "#474747" }
+        {
+            name: "Rock",
+            id: 1,
+            background: "#110133",
+            color: "white",
+            genreId: 152
+        },
+        {
+            name: "Pop",
+            id: 2,
+            background: "#293462",
+            color: "white",
+            genreId: 132
+        },
+        {
+            name: "Country",
+            id: 3,
+            background: "#4b8e8d",
+            color: "white",
+            genreId: 84
+        },
+        {
+            name: "Hip Hop",
+            id: 4,
+            background: "#00918e",
+            color: "white",
+            genreId: 116
+        },
+        {
+            name: "Dance",
+            id: 5,
+            background: "#4dd599",
+            color: "#474747"
+        },
+        {
+            name: "Indie",
+            id: 6,
+            background: "#a8ffa3",
+            color: "#474747"
+        },
+        {
+            name: "Reggae",
+            id: 7,
+            genreId: 144
+        },
+        {
+            name: "R&B",
+            id: 8,
+            genreId: 165
+        },
+        {
+            name: "Alternative",
+            id: 9,
+            genreId: 85
+        },
+        {
+            name: "Electro",
+            id: 10,
+            genreId: 106
+        },
+        {
+            name: "Folk",
+            id: 11,
+            genreId: 466
+        },
+        {
+            name: "Jazz",
+            id: 12,
+            genreId: 129
+        },
+        {
+            name: "Salsa",
+            id: 13,
+            genreId: 67
+        },
+        {
+            name: "Traditional Mexicano",
+            id: 14,
+            genreId: 65
+        },
+        {
+            name: "Classical",
+            id: 15,
+            genreId: 98
+        },
+        {
+            name: "Metal",
+            id: 16,
+            genreId: 464
+        },
+        {
+            name: "Soul & Funk",
+            id: 17,
+            genreId: 169
+        },
+        {
+            name: "Blues",
+            id: 18,
+            genreId: 153
+        },
+        {
+            name: "Latin Music",
+            id: 19,
+            genreId: 197
+        }
     ]
 }
